0738d7fcab412cb1e653ee1bb80620ebf0269c50
David Stevenson <david@ytfc.com>
2021-02-03 20:43:43 +0000

    Render actual output pages from Markdown and Html

--- Sources/SongsToTheSiren/Pages/AboutPage.swift
+++ Sources/SongsToTheSiren/Pages/AboutPage.swift
@@ -4,4 +4,13 @@ struct AboutPage: Page {
     let fileUtils: FileUtils
     var dirPath = ["about"]
     var markdownNames = ["about"]
+
+    func pageContent(markdown: [String : HtmlNode]) -> HtmlNode {
+        markdown["about"]!
+    }
+
+    func pageTitle() -> String {
+        return "About 'Songs to the Siren'"
+    }
+
 }
