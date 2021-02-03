006a0a9a3251c0489e7e0aba9262a43b6a194084
David Stevenson <david@ytfc.com>
2021-02-03 00:10:33 +0000

    Remove whitespace-only lines

--- Sources/SongsToTheSiren/Pages/Page+Markdown.swift
+++ Sources/SongsToTheSiren/Pages/Page+Markdown.swift
@@ -5,7 +5,7 @@ extension Page {
     // Loads all the markdown files, converting them to swift-html node trees
     // Does not apply shortcut detection, you should apply them when
     // actually required (e.g. when processing a song article)
-    
+
     func loadMarkdown() -> [String: HtmlNode] {
         // TODO implement
         return [String: HtmlNode]()
