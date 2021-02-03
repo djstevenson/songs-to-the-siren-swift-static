78855bbeb47816f418cfdb3a180ea48097475bb3
David Stevenson <david@ytfc.com>
2021-02-03 22:44:36 +0000

    Add list of songs, and a struct for mapping each to next/prev

---
+++ Sources/SongsToTheSiren/SongData/SongList.swift
@@ -0,0 +1,13 @@
+struct SongsList {
+    let songs: [Song] = [
+        Song.imNotInLove(),
+        Song.numberOneSongInHeaven(),
+        Song.planetOfSound(),
+        Song.clearestBlue(),
+        Song.tinyFeet(),
+        Song.lostInThePlot(),
+        Song.intoU(),
+        Song.supervixen(),
+        Song.songToTheSiren()
+    ]
+}
---
+++ Sources/SongsToTheSiren/SongData/SongMap.swift
@@ -0,0 +1,4 @@
+struct SongMap {
+    let next: Dictionary<Int, Song>
+    let prev: Dictionary<Int, Song>
+}
