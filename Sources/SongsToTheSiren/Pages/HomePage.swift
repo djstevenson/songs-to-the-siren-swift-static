05c8c01e458efbf7fca8d3da428655dcad45b8a4
David Stevenson <david@ytfc.com>
2021-02-14 04:25:42 +0000

    Factor out common code into a protocol extension

--- Sources/SongsToTheSiren/Pages/HomePage.swift
+++ Sources/SongsToTheSiren/Pages/HomePage.swift
@@ -53,37 +53,6 @@ struct HomePage: Page {
         )
     }
 
-
-    private func fullPanelSong(_ song: Song) -> HtmlNode {
-        // TODO this is hacky
-        let dummy = Dictionary<Int, Song>()
-        let songPage = SongPage(fileUtils: fileUtils, song: song, songMap: SongList.SongMap(next:dummy, prev: dummy))
-        let md = songPage.loadMarkdown()
-
-        return .section(attributes: [.class("song row rounded")],
-            .header(attributes: [.class("col-12 p-0")],
-                .a(
-                    attributes: [ songHref(song) ], // TODO higher resolution images for retina displays
-                    .img(attributes: [.class("rounded float-left mr-3 mb-1"), .alt("Record sleeve image"), .src("/song/\(song.dir)/artwork-1x.jpg"), .width(160), .height(160)])
-                ),
-                .h2(attributes: [.class("title rounded-top")],
-                    .text(song.title)
-                ),
-                .h3(attributes: [.class("artist")],
-                    .text(song.artist)
-                ),
-                .h3(attributes: [.class("artist")],
-                    .span(attributes:[], .fragment(song.country.map { .text($0.rawValue + " ") } ))
-                ),
-                .h4(attributes: [.class("release")], .text(song.released))
-            ),
-            .div(
-                attributes: [.class("description")],
-                md["summary"]!
-            )
-        )
-    }
-
     private func listingSong(_ song: Song) -> ChildOf<Tag.Ul> {
         .li(
             .a(
@@ -92,11 +61,6 @@ struct HomePage: Page {
             ),
             .text(" - \(song.artist): Timestamp from somewhere")
         )
-    }
-
-    // TODO Should this be in song class? Probs ok here, actually
-    private func songHref(_ song: Song) -> Attribute<Tag.A> {
-        return .href("/song/\(song.dir)/")
     }
 
     func writeExtras() -> Void {}
--- Sources/SongsToTheSiren/Pages/TagPage.swift
+++ Sources/SongsToTheSiren/Pages/TagPage.swift
@@ -30,39 +30,5 @@ struct TagPage: Page {
         )
     }
 
-
-    private func fullPanelSong(_ song: Song) -> HtmlNode {
-        // TODO this is hacky
-        let dummy = Dictionary<Int, Song>()
-        let songPage = SongPage(fileUtils: fileUtils, song: song, songMap: SongList.SongMap(next:dummy, prev: dummy))
-        let md = songPage.loadMarkdown()
-
-        // TODO THIS NEEDS DRYing
-        return .section(
-            .header(
-                .a(
-                    attributes: [ // TODO grep for index.html, this is re-used in loads of places
-                        .href("/song/\(song.dir)/")
-                    ],
-                    .img(attributes: [.class("artwork"), .alt("Record sleeve image"), .src("x.jpg"), .width(160), .height(160)]),
-                    .h2(.text(song.title))
-                ),
-                .h3(
-                    attributes: [.class("artist")],
-                    .text(song.artist),
-                    .span(
-                        attributes: [.class("country")],
-                        .text(song.country.map { $0.rawValue }.joined(separator: " "))
-                    )
-                ),
-                .h4(attributes: [.class("release")], .text(song.released))
-            ),
-            .div(
-                attributes: [.class("description")],
-                md["summary"]!
-            )
-        )
-    }
-
     func writeExtras() -> Void {}
 }
