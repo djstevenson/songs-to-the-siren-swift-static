c59976b282c52cf3e761b90c35768db849b3005a
David Stevenson <david@ytfc.com>
2021-03-06 02:12:17 +0000

    New songs

---
+++ Sources/SongsToTheSiren/SongData/Songs/LieDreamOfACasinoSoul.swift
@@ -0,0 +1,39 @@
+import Foundation
+extension Song {
+    static func lieDreamOfACasinoSoul() -> Song {
+        Song (
+            id:        62,
+            style:     .fullPanel,
+            dir:       String.folderFromFunctionName(name: #function),
+            artist:    "The Fall",
+            title:     "Lie Dream of a Casino Soul",
+            album:     "Single (1981). Also on CD re-issue of Slates mini-LP",
+            released:  "1981",
+            maxRez:    4,
+            createdAt: Date(timeIntervalSince1970: 1594519001),
+            updatedAt: Date(timeIntervalSince1970: 1594519001),
+            tags:      [.decade1980s, .manchester],
+            country:   [.uk],
+            links:     SongLinks(links: [
+                SongLink(
+                    id:        "default",
+                    embedText: "Lie Dream of a Casino Soul",
+                    listText:  "Lie Dream of a Casino Soul - The Fall",
+                    linkType:  .youtubeVideo(code: "vqBrAFPG-DE")
+                ),
+                SongLink(
+                    id:        "wigan-casino",
+                    embedText: "Wigan Casino",
+                    listText:  "The Wigan Casino (Wikipedia)",
+                    linkType:  .wikipedia(page: "Wigan_Casino")
+                ),
+                SongLink(
+                    id:        "let-your-heart-dance",
+                    embedText: "Let Your Heart Dance",
+                    listText:  "Let Your Heart Dance - Secret Affair",
+                    linkType:  .youtubeLink(code: "Xgt3-ao4nF4")
+                )
+            ])
+        )
+    }
+}
