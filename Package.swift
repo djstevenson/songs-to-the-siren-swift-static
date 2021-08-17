9233bc3e7921ea5bbfa5db46d813586f6e978e11
David Stevenson <david@ytfc.com>
2021-08-18 00:07:43 +0100

    Whitespace diffs only, for alignment

--- Package.swift
+++ Package.swift
@@ -12,19 +12,19 @@ let package = Package(
         .executable(name: "songs-to-the-siren", targets: ["SongsToTheSiren"])
     ],
     dependencies: [
-        .package(                 url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.3")),
-        .package(                 url: "https://github.com/johnxnguyen/Down",            .upToNextMinor(from: "0.11.0")),
-        .package(                 url: "https://github.com/pointfreeco/swift-html",      .upToNextMinor(from: "0.4.0")),
-        .package(                 url: "https://github.com/pointfreeco/swift-parsing",   .upToNextMinor(from: "0.1.2"))
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.3")),
+        .package(url: "https://github.com/johnxnguyen/Down",            .upToNextMinor(from: "0.11.0")),
+        .package(url: "https://github.com/pointfreeco/swift-html",      .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/pointfreeco/swift-parsing",   .upToNextMinor(from: "0.1.2"))
     ],
     targets: [
         .target(
             name: "SongsToTheSiren",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Down",    package: "Down"),
-                .product(name: "Html",    package: "swift-html"),
-                .product(name: "Parsing", package: "swift-parsing")
+                .product(name: "Down",           package: "Down"),
+                .product(name: "Html",           package: "swift-html"),
+                .product(name: "Parsing",        package: "swift-parsing")
             ]),
         .testTarget(
             name: "SongsToTheSirenTests",
