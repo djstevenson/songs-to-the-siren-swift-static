502aa960a854951d54a8a6c6e87e14c19f8e01b3
David Stevenson <david@ytfc.com>
2024-11-16 18:33:52 +0000

    Node deps updates

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
         .executable(name: "songs-to-the-siren", targets: ["SongsToTheSiren"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser",  .upToNextMinor(from: "1.3.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser",  .upToNextMinor(from: "1.5.0")),
         .package(url: "https://github.com/johnxnguyen/Down",             .upToNextMinor(from: "0.11.0")),
-        .package(url: "https://github.com/pointfreeco/swift-html",       .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/pointfreeco/swift-html",       .upToNextMinor(from: "0.5.0")),
         .package(url: "https://github.com/pointfreeco/swift-parsing",    .upToNextMinor(from: "0.13.0"))
     ],
     targets: [
